140207077861ef574508131a32c46b6ecbd6028d
Or Esperansa <6or2005@gmail.com>
2025-09-26 22:15:36 +0300

    New Ascii Logo

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -813,12 +813,18 @@ var RollMoney = (() => {
       (function() {
         "use strict";
         console.log(`
-__       __   ______   __    __  ________  __      __        __       __   ______   __    __  ________  _______
-|       /   /       |    |  |        |      /        |       /   /       |    /  |        |       | $$   /  $$|  $$$$$$| $$ | $$| $$$$$$$$ $$  /  $$      | $$   /  $$|  $$$$$$| $$ /  $$| $$$$$$$$| $$$$$$$| $$$ /  $$$| $$  | $$| $$$| $$| $$__      $$/  $$       | $$$ /  $$$| $$__| $$| $$/  $$ | $$__    | $$__| $$
-| $$$$  $$$$| $$  | $$| $$$$ $$| $$        $$  $$        | $$$$  $$$$| $$    $$| $$  $$  | $$     | $$    $$
-| $$$$ $$ $$| $$  | $$| $$$$ $$| $$$$$       $$$$         | $$$$ $$ $$| $$$$$$$$| $$$$$  | $$$$$   | $$$$$$$| $$ $$$| $$| $$__/ $$| $$ $$$$| $$_____     | $$          | $$ $$$| $$| $$  | $$| $$ $$ | $$_____ | $$  | $$
-| $$  $ | $$ $$    $$| $$  $$$| $$         | $$          | $$  $ | $$| $$  | $$| $$  $$| $$     | $$  | $$
- $$      $$  $$$$$$  $$   $$ $$$$$$$$     $$           $$      $$ $$   $$ $$   $$ $$$$$$$$ $$   $$
+MM    MM  OOOOO  NN   NN EEEEEEE YY   YY 
+MMM  MMM OO   OO NNN  NN EE      YY   YY 
+MM MM MM OO   OO NN N NN EEEEE    YYYYY  
+MM    MM OO   OO NN  NNN EE        YYY   
+MM    MM  OOOO0  NN   NN EEEEEEE   YYY   
+                                         
+MM    MM   AAA   KK  KK EEEEEEE RRRRRR   
+MMM  MMM  AAAAA  KK KK  EE      RR   RR  
+MM MM MM AA   AA KKKK   EEEEE   RRRRRR   
+MM    MM AAAAAAA KK KK  EE      RR  RR   
+MM    MM AA   AA KK  KK EEEEEEE RR   RR  
+                                         
 `);
         window.MarketItemScraper = new MarketItemScraper();
       })();
--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-// ==UserScript==
-// @name         Market Item Scraper
-// @namespace    https://github.com/Esperansa1/RollMoney
-// @version      1.0.0
-// @description  Automated market item scraper with filtering and withdrawal functionality
-// @author       MakeMoney
-// @match        *://*/*
-// @grant        none
-// @updateURL    https://raw.githubusercontent.com/Esperansa1/RollMoney/dev/dist/index.bundle.js
-// @downloadURL  https://raw.githubusercontent.com/Esperansa1/RollMoney/dev/dist/index.bundle.js
-// ==/UserScript==
-
 import { MarketItemScraper } from './src/market-scraper.js';
 
 (function() {
     'use strict';
     console.log(`
-__       __   ______   __    __  ________  __      __        __       __   ______   __    __  ________  _______
-|  \     /  \ /      \ |  \  |  \|        \|  \    /  \      |  \     /  \ /      \ |  \  /  \|        \|       \
-| $$\   /  $$|  $$$$$$\| $$\ | $$| $$$$$$$$ \$$\  /  $$      | $$\   /  $$|  $$$$$$\| $$ /  $$| $$$$$$$$| $$$$$$$\
-| $$$\ /  $$$| $$  | $$| $$$\| $$| $$__      \$$\/  $$       | $$$\ /  $$$| $$__| $$| $$/  $$ | $$__    | $$__| $$
-| $$$$\  $$$$| $$  | $$| $$$$\ $$| $$  \      \$$  $$        | $$$$\  $$$$| $$    $$| $$  $$  | $$  \   | $$    $$
-| $$\$$ $$ $$| $$  | $$| $$\$$ $$| $$$$$       \$$$$         | $$\$$ $$ $$| $$$$$$$$| $$$$$\  | $$$$$   | $$$$$$$\
-| $$ \$$$| $$| $$__/ $$| $$ \$$$$| $$_____     | $$          | $$ \$$$| $$| $$  | $$| $$ \$$\ | $$_____ | $$  | $$
-| $$  \$ | $$ \$$    $$| $$  \$$$| $$     \    | $$          | $$  \$ | $$| $$  | $$| $$  \$$\| $$     \| $$  | $$
- \$$      \$$  \$$$$$$  \$$   \$$ \$$$$$$$$     \$$           \$$      \$$ \$$   \$$ \$$   \$$ \$$$$$$$$ \$$   \$$
+MM    MM  OOOOO  NN   NN EEEEEEE YY   YY 
+MMM  MMM OO   OO NNN  NN EE      YY   YY 
+MM MM MM OO   OO NN N NN EEEEE    YYYYY  
+MM    MM OO   OO NN  NNN EE        YYY   
+MM    MM  OOOO0  NN   NN EEEEEEE   YYY   
+                                         
+MM    MM   AAA   KK  KK EEEEEEE RRRRRR   
+MMM  MMM  AAAAA  KK KK  EE      RR   RR  
+MM MM MM AA   AA KKKK   EEEEE   RRRRRR   
+MM    MM AAAAAAA KK KK  EE      RR  RR   
+MM    MM AA   AA KK  KK EEEEEEE RR   RR  
+                                         
 `)
 
     window.MarketItemScraper = new MarketItemScraper();
