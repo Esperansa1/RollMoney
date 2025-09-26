d9051226e28d422908e652c882fd98c3b0c6b98e
Or Esperansa <6or2005@gmail.com>
2025-09-26 21:58:41 +0300

    Added tampermonkey header for index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+// ==UserScript==
+// @name         Market Item Scraper
+// @namespace    https://github.com/Esperansa1/RollMoney
+// @version      1.0.0
+// @description  Automated market item scraper with filtering and withdrawal functionality
+// @author       MakeMoney
+// @match        *://*/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/Esperansa1/RollMoney/dev/dist/index.bundle.js
+// @downloadURL  https://raw.githubusercontent.com/Esperansa1/RollMoney/dev/dist/index.bundle.js
+// ==/UserScript==
+
 import { MarketItemScraper } from './src/market-scraper.js';
 
 (function() {
     'use strict';
     console.log(`
-__       __   ______   __    __  ________  __      __        __       __   ______   __    __  ________  _______  
-|  \     /  \ /      \ |  \  |  \|        \|  \    /  \      |  \     /  \ /      \ |  \  /  \|        \|       \ 
+__       __   ______   __    __  ________  __      __        __       __   ______   __    __  ________  _______
+|  \     /  \ /      \ |  \  |  \|        \|  \    /  \      |  \     /  \ /      \ |  \  /  \|        \|       \
 | $$\   /  $$|  $$$$$$\| $$\ | $$| $$$$$$$$ \$$\  /  $$      | $$\   /  $$|  $$$$$$\| $$ /  $$| $$$$$$$$| $$$$$$$\
 | $$$\ /  $$$| $$  | $$| $$$\| $$| $$__      \$$\/  $$       | $$$\ /  $$$| $$__| $$| $$/  $$ | $$__    | $$__| $$
 | $$$$\  $$$$| $$  | $$| $$$$\ $$| $$  \      \$$  $$        | $$$$\  $$$$| $$    $$| $$  $$  | $$  \   | $$    $$
